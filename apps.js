c595cdb8bbb7f5756d706b359cd4be9258a19741
traven <travenblevins@gmail.com>
2024-01-15 17:37:33 -0700

    first commit

---
+++ apps.js
@@ -0,0 +1,6 @@
+
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$('li:even').css('background-color', '#ccc');
+$('li:odd').css('background-color', '#aaa');
+$('input').val('YourFirstName');
